7e145e1d91fc8841b3b95e4d657fc0332bf26c2a
Daniel Rzehak <interraptor@yahoo.de>
2011-08-02 17:42:57 +0200

    neuer table für SG und dummy dateien fpr alle Managment Objekte

--- SQL Dumps/UniManager02_08_2011_RZ.sql
+++ SQL Dumps/UniManager02_08_2011_RZ.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 02. August 2011 um 16:42
+-- Erstellungszeit: 02. August 2011 um 17:41
 -- Server Version: 5.5.8
 -- PHP-Version: 5.3.5
 
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `modul` (
   `modul_effort` varchar(600) NOT NULL,
   PRIMARY KEY (`modul_modul-id`),
   UNIQUE KEY `modul_name` (`modul_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Daten für Tabelle `modul`
@@ -58,6 +58,29 @@ CREATE TABLE IF NOT EXISTS `modul` (
 INSERT INTO `modul` (`modul_descr`, `modul_name`, `modul_targetdate`, `modul_modul-id`, `modul_status`, `modul_person-id`, `modul_last_cha`, `modul_mname`, `modul_quarantor`, `modul_institut`, `modul_duration`, `modul_qualifytarget`, `modul_content`, `modul_literature`, `modul_teachform`, `modul_required`, `modul_frequency`, `modul_usability`, `modul_lp`, `modul_conditionforln`, `modul_effort`) VALUES
 ('Informationssysteme', 'Informationssysteme', 'Mittwoch 11-12:30 Uhr', 1, 'Genehmigt', 1, '2011-07-04', 'Informationssysteme', 'Dr. Heinricht Jasper', 'Institut für Informatik', 2, ' Die Studierenden sollen die Prinzipien relationaler Datenbanksysteme  kennen und den Entwurfsprozess beherrschen sowie betriebliche Informationssysteme im Team konzipieren, entwerfen, realisieren und  einführen können', ' Datenmodellierung und Datenmanagement, insbesondere das relationale Datenmodell einschließlich Algebra und Kalkül und postrelationale  Datenmodelle. Datenbankdesign, vom Entity-Relationship-Modell über  Transformationen, logischem Design und Normalisierung zum physischen Design. Datenbankadministration, SQL und Metadaten. Integrität: logische und physische Integrität, Synchronisation und Transaktionen. Architektur, Schnittstellen und Funktionen von Datenbankmanagementsystemen. Im Praktikum ist  ein Datenbanksystem im Team zu  erstellen. Informationssysteme zur Unterstützung betrieblicher / organisatorischer Prozesse, Prozessmodellierung, Konzeption, Umsetzung in  UML, Skriptsprachen, Application-/Webserver, Konstruktion eines Webbasierten Informationssystems im Team', ' Kemper/Eickler: Datenbanksysteme, Oldenbourg; Elmasri/Navathe:  Grundlagen von Datenbanksystemen, Addison-Wesley: Connolly,  Begg, Database Systems, Addison-Wesley, Carl Steinweg: Management der Software-Entwicklung, Teubner', ' Vorlesungen (3 SWS), Übungen (1 SWS), Praktikum DBMS (1 SWS),  Praktikum Informationssysteme (1 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend den Inhalten der Module Grundlagen der Informatik und Softwareentwicklung', 'Jährlich zum Wintersemester  ', ' Bachelorstudiengänge Network Computing und Engineering & Computin', 9, ' Die Modulprüfung besteht aus einer Klausurarbeit im Umfang von 90  Minuten (Datenbanksysteme), und einer alternativen Prüfungsleistung  (erfolgreiche Abnahme eines Informationssystems)', ' Der Zeitaufwand beträgt 270 h und setzt sich aus 90 h Präsenzzeit und  180 h Selbststudium zusammen. Letzteres umfasst die Vor- und Nachbereitung der Lehrveranstaltungen, die Einarbeitung in eine Skriptsprache und das Aufsetzen der IS-Infrastruktur, die Ausarbeitung der Praktikumsaufgaben im Team, die Vorbereitung auf die schriftliche und die  mündliche Prüfung sowie die Präsentation des Informationssystems'),
 (' Mensch-Maschine-Kommunikation', ' Mensch-Maschine-Kommunikation', 'V: Do 15:15-17:15 U: Mi 9:15-10:45', 2, 'Bearbeitung', 2, '2011-07-04', ' Mensch-Maschine-Kommunikation', 'Prof. Dr. Bernhard Jung', 'Institut für Informatik', 2, ' Erwerb grundlegender Kenntnisse der Interaktionsformen für die  Kommunikation mit Computern.   Fähigkeit zur Anwendung dieser Kenntnisse bei der Gestaltung von  Benutzungsschnittstellen.   Einblicke in das wissenschaftliche Gebiet der Mensch-MaschineKommunikation. ', ' - Kognitive Aspekte der MMK (Wahrnehmung, Gedächtnis, Handlungsprozesse)  - Interaktionsformen  - Grafische Dialogsysteme  - Unterstützung von Kommunikation und Kollaboration  - Affektive Benutzungsschnittstellen  - Neue Paradigmen der MMK (z.B. Virtual & Augmented Reality,  Ubiquitous Computing, Agenten-basierte Schnittstellen, Tangible  Media)', ' M. Dahm. Grundlagen der Mensch-Computer-Interaktion. Pearson  Studium. 2006.  Alan Dix, Janet E. Finlay, Gregory D. Abowd, Russell Beale. HumanComputer Interaction, 3rd Edition. Prentice Hall, 2004.  Jennifer Preece, Yvonne Rogers, Helen Sharp.  Interaction Design:  Beyond Human-Computer Interaction. John Wiley & Sons, 2002', ' Seminaristische Vorlesung (2 SWS), Projektseminar (2 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend des Inhalts des  Moduls Grundlagen der Informati', 'Jährlich zum Wintersemester  ', ' Diplomstudiengang Angewandte Mathematik, Bachelorstudiengänge  Network Computing und Engineering & Computing', 6, ' Leistungspunkte werden nach bestandener mündlicher Prüfungsleistung im Umfang von 30 Minuten und bestandener alternativer Prü- fungsleistung (Bearbeitung eines Gruppenprojekts) vergeben. ', ' Der Zeitaufwand beträgt 180 h und setzt sich aus 60 h Präsenzzeit  und 120 h Selbststudium zusammen. Letzteres umfasst die Vor- und  Nachbereitung der Lehrveranstaltungen, die Arbeit an einem Gruppenprojekt sowie die Prüfungsvorbereitung');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `studiengang`
+--
+
+CREATE TABLE IF NOT EXISTS `studiengang` (
+  `sg_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sg_name` varchar(20) NOT NULL,
+  `sg_po` text NOT NULL,
+  `sg_so` text NOT NULL,
+  `sg_createdate` date NOT NULL,
+  `sg_modulhandbuch` text NOT NULL,
+  PRIMARY KEY (`sg_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Daten für Tabelle `studiengang`
+--
+
+INSERT INTO `studiengang` (`sg_id`, `sg_name`, `sg_po`, `sg_so`, `sg_createdate`, `sg_modulhandbuch`) VALUES
+(1, 'Bachelor Network Com', 'Inhalt nicht verfügbar', 'Inhalt nicht verfügbar', '1999-10-01', 'Inhalt nicht verfügbar');
 
 -- --------------------------------------------------------
 
@@ -83,4 +106,4 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`user_id`, `user_loginname`, `user_passwort`, `user_vorname`, `user_nachname`, `user_accesslvl`, `user_lastlogin`) VALUES
 (1, 'shexbeer', '098f6bcd4621d373cade4e832627b4f6', 'Christian', 'Seidel', 100, '1308593834'),
 (2, 'sebi', '4fc477a3636c998c46817ac7534f0c48', 'Sebastian', 'Gasch', 100, '1309779230'),
-(3, 'sid', '709218c83dcb3d5597e6bf31e2eb1054', 'Daniel', 'Rzehak', 100, '');
+(3, 'sid', '709218c83dcb3d5597e6bf31e2eb1054', 'Daniel', 'Rzehak', 100, '1312296252');
