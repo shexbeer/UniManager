9d919c052569d176f1a2ff9bb060c8fc3dfb1444
Daniel Rzehak <interraptor@yahoo.de>
2011-08-05 16:35:51 +0200

    Endresultat für heute aktuallisierte DB dabei

--- SQL Dumps/UniManager05_08_2011_RZ.sql
+++ SQL Dumps/UniManager05_08_2011_RZ.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 05. August 2011 um 14:12
+-- Erstellungszeit: 05. August 2011 um 16:34
 -- Server Version: 5.5.8
 -- PHP-Version: 5.3.5
 
@@ -26,14 +26,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `modul` (
-  `modul_descr` varchar(1500) NOT NULL,
   `modul_name` varchar(40) NOT NULL,
   `modul_targetdate` varchar(50) NOT NULL,
   `modul_id` int(10) NOT NULL AUTO_INCREMENT,
   `modul_status` set('Bearbeitung','Erstellt','Genehmigt') NOT NULL DEFAULT 'Bearbeitung',
   `modul_person_id` int(10) NOT NULL,
   `modul_last_cha` date NOT NULL,
-  `modul_mname` varchar(40) NOT NULL,
   `modul_quarantor` varchar(40) NOT NULL,
   `modul_institut` varchar(50) NOT NULL,
   `modul_duration` int(3) NOT NULL,
@@ -55,9 +53,9 @@ CREATE TABLE IF NOT EXISTS `modul` (
 -- Daten für Tabelle `modul`
 --
 
-INSERT INTO `modul` (`modul_descr`, `modul_name`, `modul_targetdate`, `modul_id`, `modul_status`, `modul_person_id`, `modul_last_cha`, `modul_mname`, `modul_quarantor`, `modul_institut`, `modul_duration`, `modul_qualifytarget`, `modul_content`, `modul_literature`, `modul_teachform`, `modul_required`, `modul_frequency`, `modul_usability`, `modul_lp`, `modul_conditionforln`, `modul_effort`) VALUES
-('Informationssysteme', 'Informationssysteme', 'Mittwoch 11-12:30 Uhr', 1, 'Genehmigt', 1, '2011-07-04', 'Informationssysteme', 'Dr. Heinricht Jasper', 'Institut für Informatik', 2, ' Die Studierenden sollen die Prinzipien relationaler Datenbanksysteme  kennen und den Entwurfsprozess beherrschen sowie betriebliche Informationssysteme im Team konzipieren, entwerfen, realisieren und  einführen können', ' Datenmodellierung und Datenmanagement, insbesondere das relationale Datenmodell einschließlich Algebra und Kalkül und postrelationale  Datenmodelle. Datenbankdesign, vom Entity-Relationship-Modell über  Transformationen, logischem Design und Normalisierung zum physischen Design. Datenbankadministration, SQL und Metadaten. Integrität: logische und physische Integrität, Synchronisation und Transaktionen. Architektur, Schnittstellen und Funktionen von Datenbankmanagementsystemen. Im Praktikum ist  ein Datenbanksystem im Team zu  erstellen. Informationssysteme zur Unterstützung betrieblicher / organisatorischer Prozesse, Prozessmodellierung, Konzeption, Umsetzung in  UML, Skriptsprachen, Application-/Webserver, Konstruktion eines Webbasierten Informationssystems im Team', ' Kemper/Eickler: Datenbanksysteme, Oldenbourg; Elmasri/Navathe:  Grundlagen von Datenbanksystemen, Addison-Wesley: Connolly,  Begg, Database Systems, Addison-Wesley, Carl Steinweg: Management der Software-Entwicklung, Teubner', ' Vorlesungen (3 SWS), Übungen (1 SWS), Praktikum DBMS (1 SWS),  Praktikum Informationssysteme (1 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend den Inhalten der Module Grundlagen der Informatik und Softwareentwicklung', 'Jährlich zum Wintersemester  ', ' Bachelorstudiengänge Network Computing und Engineering & Computin', 9, ' Die Modulprüfung besteht aus einer Klausurarbeit im Umfang von 90  Minuten (Datenbanksysteme), und einer alternativen Prüfungsleistung  (erfolgreiche Abnahme eines Informationssystems)', ' Der Zeitaufwand beträgt 270 h und setzt sich aus 90 h Präsenzzeit und  180 h Selbststudium zusammen. Letzteres umfasst die Vor- und Nachbereitung der Lehrveranstaltungen, die Einarbeitung in eine Skriptsprache und das Aufsetzen der IS-Infrastruktur, die Ausarbeitung der Praktikumsaufgaben im Team, die Vorbereitung auf die schriftliche und die  mündliche Prüfung sowie die Präsentation des Informationssystems'),
-(' Mensch-Maschine-Kommunikation', ' Mensch-Maschine-Kommunikation', 'V: Do 15:15-17:15 U: Mi 9:15-10:45', 2, 'Bearbeitung', 2, '2011-07-04', ' Mensch-Maschine-Kommunikation', 'Prof. Dr. Bernhard Jung', 'Institut für Informatik', 2, ' Erwerb grundlegender Kenntnisse der Interaktionsformen für die  Kommunikation mit Computern.   Fähigkeit zur Anwendung dieser Kenntnisse bei der Gestaltung von  Benutzungsschnittstellen.   Einblicke in das wissenschaftliche Gebiet der Mensch-MaschineKommunikation. ', ' - Kognitive Aspekte der MMK (Wahrnehmung, Gedächtnis, Handlungsprozesse)  - Interaktionsformen  - Grafische Dialogsysteme  - Unterstützung von Kommunikation und Kollaboration  - Affektive Benutzungsschnittstellen  - Neue Paradigmen der MMK (z.B. Virtual & Augmented Reality,  Ubiquitous Computing, Agenten-basierte Schnittstellen, Tangible  Media)', ' M. Dahm. Grundlagen der Mensch-Computer-Interaktion. Pearson  Studium. 2006.  Alan Dix, Janet E. Finlay, Gregory D. Abowd, Russell Beale. HumanComputer Interaction, 3rd Edition. Prentice Hall, 2004.  Jennifer Preece, Yvonne Rogers, Helen Sharp.  Interaction Design:  Beyond Human-Computer Interaction. John Wiley & Sons, 2002', ' Seminaristische Vorlesung (2 SWS), Projektseminar (2 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend des Inhalts des  Moduls Grundlagen der Informati', 'Jährlich zum Wintersemester  ', ' Diplomstudiengang Angewandte Mathematik, Bachelorstudiengänge  Network Computing und Engineering & Computing', 6, ' Leistungspunkte werden nach bestandener mündlicher Prüfungsleistung im Umfang von 30 Minuten und bestandener alternativer Prü- fungsleistung (Bearbeitung eines Gruppenprojekts) vergeben. ', ' Der Zeitaufwand beträgt 180 h und setzt sich aus 60 h Präsenzzeit  und 120 h Selbststudium zusammen. Letzteres umfasst die Vor- und  Nachbereitung der Lehrveranstaltungen, die Arbeit an einem Gruppenprojekt sowie die Prüfungsvorbereitung');
+INSERT INTO `modul` (`modul_name`, `modul_targetdate`, `modul_id`, `modul_status`, `modul_person_id`, `modul_last_cha`, `modul_quarantor`, `modul_institut`, `modul_duration`, `modul_qualifytarget`, `modul_content`, `modul_literature`, `modul_teachform`, `modul_required`, `modul_frequency`, `modul_usability`, `modul_lp`, `modul_conditionforln`, `modul_effort`) VALUES
+('Informationssysteme', 'Mittwoch 11-12:30 Uhr', 1, 'Genehmigt', 1, '2011-07-04', 'Dr. Heinricht Jasper', 'Institut für Informatik', 2, ' Die Studierenden sollen die Prinzipien relationaler Datenbanksysteme  kennen und den Entwurfsprozess beherrschen sowie betriebliche Informationssysteme im Team konzipieren, entwerfen, realisieren und  einführen können', ' Datenmodellierung und Datenmanagement, insbesondere das relationale Datenmodell einschließlich Algebra und Kalkül und postrelationale  Datenmodelle. Datenbankdesign, vom Entity-Relationship-Modell über  Transformationen, logischem Design und Normalisierung zum physischen Design. Datenbankadministration, SQL und Metadaten. Integrität: logische und physische Integrität, Synchronisation und Transaktionen. Architektur, Schnittstellen und Funktionen von Datenbankmanagementsystemen. Im Praktikum ist  ein Datenbanksystem im Team zu  erstellen. Informationssysteme zur Unterstützung betrieblicher / organisatorischer Prozesse, Prozessmodellierung, Konzeption, Umsetzung in  UML, Skriptsprachen, Application-/Webserver, Konstruktion eines Webbasierten Informationssystems im Team', ' Kemper/Eickler: Datenbanksysteme, Oldenbourg; Elmasri/Navathe:  Grundlagen von Datenbanksystemen, Addison-Wesley: Connolly,  Begg, Database Systems, Addison-Wesley, Carl Steinweg: Management der Software-Entwicklung, Teubner', ' Vorlesungen (3 SWS), Übungen (1 SWS), Praktikum DBMS (1 SWS),  Praktikum Informationssysteme (1 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend den Inhalten der Module Grundlagen der Informatik und Softwareentwicklung', 'Jährlich zum Wintersemester  ', ' Bachelorstudiengänge Network Computing und Engineering & Computin', 9, ' Die Modulprüfung besteht aus einer Klausurarbeit im Umfang von 90  Minuten (Datenbanksysteme), und einer alternativen Prüfungsleistung  (erfolgreiche Abnahme eines Informationssystems)', ' Der Zeitaufwand beträgt 270 h und setzt sich aus 90 h Präsenzzeit und  180 h Selbststudium zusammen. Letzteres umfasst die Vor- und Nachbereitung der Lehrveranstaltungen, die Einarbeitung in eine Skriptsprache und das Aufsetzen der IS-Infrastruktur, die Ausarbeitung der Praktikumsaufgaben im Team, die Vorbereitung auf die schriftliche und die  mündliche Prüfung sowie die Präsentation des Informationssystems'),
+(' Mensch-Maschine-Kommunikation', 'V: Do 15:15-17:15 U: Mi 9:15-10:45', 2, 'Bearbeitung', 2, '2011-07-04', 'Prof. Dr. Bernhard Jung', 'Institut für Informatik', 2, ' Erwerb grundlegender Kenntnisse der Interaktionsformen für die  Kommunikation mit Computern.   Fähigkeit zur Anwendung dieser Kenntnisse bei der Gestaltung von  Benutzungsschnittstellen.   Einblicke in das wissenschaftliche Gebiet der Mensch-MaschineKommunikation. ', ' - Kognitive Aspekte der MMK (Wahrnehmung, Gedächtnis, Handlungsprozesse)  - Interaktionsformen  - Grafische Dialogsysteme  - Unterstützung von Kommunikation und Kollaboration  - Affektive Benutzungsschnittstellen  - Neue Paradigmen der MMK (z.B. Virtual & Augmented Reality,  Ubiquitous Computing, Agenten-basierte Schnittstellen, Tangible  Media)', ' M. Dahm. Grundlagen der Mensch-Computer-Interaktion. Pearson  Studium. 2006.  Alan Dix, Janet E. Finlay, Gregory D. Abowd, Russell Beale. HumanComputer Interaction, 3rd Edition. Prentice Hall, 2004.  Jennifer Preece, Yvonne Rogers, Helen Sharp.  Interaction Design:  Beyond Human-Computer Interaction. John Wiley & Sons, 2002', ' Seminaristische Vorlesung (2 SWS), Projektseminar (2 SWS', ' Vorausgesetzt werden Kenntnisse entsprechend des Inhalts des  Moduls Grundlagen der Informati', 'Jährlich zum Wintersemester  ', ' Diplomstudiengang Angewandte Mathematik, Bachelorstudiengänge  Network Computing und Engineering & Computing', 6, ' Leistungspunkte werden nach bestandener mündlicher Prüfungsleistung im Umfang von 30 Minuten und bestandener alternativer Prü- fungsleistung (Bearbeitung eines Gruppenprojekts) vergeben. ', ' Der Zeitaufwand beträgt 180 h und setzt sich aus 60 h Präsenzzeit  und 120 h Selbststudium zusammen. Letzteres umfasst die Vor- und  Nachbereitung der Lehrveranstaltungen, die Arbeit an einem Gruppenprojekt sowie die Prüfungsvorbereitung');
 
 -- --------------------------------------------------------
 
@@ -69,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `modulaufstellung` (
   `mauf_rowid` int(11) NOT NULL AUTO_INCREMENT,
   `mauf_modul_id` int(11) NOT NULL,
   `mauf_sg_id` int(11) NOT NULL,
-  `mauf_plansemester` varchar(6) NOT NULL,
+  `mauf_plansemester` int(2) NOT NULL,
   `mauf_typ` varchar(20) NOT NULL,
   PRIMARY KEY (`mauf_rowid`),
   KEY `mauf_sg_id` (`mauf_sg_id`),
@@ -81,8 +79,8 @@ CREATE TABLE IF NOT EXISTS `modulaufstellung` (
 --
 
 INSERT INTO `modulaufstellung` (`mauf_rowid`, `mauf_modul_id`, `mauf_sg_id`, `mauf_plansemester`, `mauf_typ`) VALUES
-(1, 1, 1, 'SS2011', 'Bachelor'),
-(2, 2, 1, 'SS2011', 'Bachelor');
+(1, 1, 1, 1, 'Bachelor'),
+(2, 2, 1, 2, 'Bachelor');
 
 -- --------------------------------------------------------
 
--- managers/Modul_Management.php
+++ managers/Modul_Management.php
@@ -1,19 +1,19 @@
 <?php
 ### Bitte auf die Funktionsparameterbeschreibung achten ###
 class Modul_Management {
-	#Verbindung zur Datenbank einmalig beim erstellen
-	function Modul_Management(){
-		require_once("db.mysql.php");
-	}
+	//Constructor zum seperatem debuggen
+	// function Modul_Management(){
+		// require_once("UniManager/db.mysql.php");
+	// }
 	
 	#Abrufen aller Module in der Datenbank
 	function getModullist(){
 		$sql = "SELECT modul_name,modul_id FROM modul ORDER BY modul_name";
 		$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
 		var_dump($res);
-		if(!$res)$rows[result]=false
+		if(!$res)$rows['result']=false;
 		else{
-			$rows[result]=true
+			$rows['result']=true;
 			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
 			for($i=0;$i<$rnum;$i++){
 			// €nderung von shexbeer: Array wird nun nach Modul ID referenziert in unterster Ebene
@@ -31,12 +31,12 @@ class Modul_Management {
 	#wenn type="sg" dann ist Paramter Plansemester möglich um die richtige Modulaufstellung zu ermitteln
 	#liefert false falls Pramater falsch;
 	function getModuldetails(){
-		if(func_num_args==0){
+		if(func_num_args()==0){
 			$sql = "SELECT * FROM modul ORDER BY modul_name";
 			$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
-			if(!$res)$rows[result]=false;
+			if(!$res)$rows['result']=false;
 			else{
-				$rows[result]=true;
+				$rows['result']=true;
 				$rnum = mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
 				$fnum = mysql_num_fields($res);
 				#liest alle Attribute dynamisch aus --> Anpassungen der Datenbankstruktur möglich
@@ -45,23 +45,38 @@ class Modul_Management {
 			}
 			return $rows; # 2dim array
 		}
-		else{
+		else{ //wenn Parameter vorhanden ab hier Auswertung
 			$type=func_get_arg(0);
 			$id=func_get_arg(1);
-			switch($type){
+			switch($type){ // Typenunterscheidung (erster Parameter)
 				case "modul":
 					$sql = "SELECT * FROM modul WHERE modul_id=".$id;
 					$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
-					if(!$res)$rows[result]=false;//Fehlererkennung
+					if(!$res)$rows['result']=false;//Fehlererkennung
 					else{
-						$rows[result]=true;
+						$rows['result']=true;
+						if(mysql_num_rows($res)==0)return $rows;
 						$fnum = mysql_num_fields($res);
+						var_dump($fnum);
 						#liest alle Attribute dynamisch aus --> Anpassungen der Datenbankstruktur möglich
-						for($j=0;$j<$fnum;$j++) $rows[$j]=mysql_result($res,$id,$j);
+						for($j=0;$j<$fnum;$j++) $rows[$j]=mysql_result($res,0,$j);
 					}
 					return $rows;
 				case "sg":
-					return false;
+					if(func_num_args()==3)$plans =" AND mauf_plansemester=".func_get_arg(2);
+					else $plans=NULL;
+					$sql = "SELECT modul.* FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id.$plans.")ON mauf_modul_id=modul_id";
+					$res = mysql_query($sql);
+					if(!$res)$rows['result']=false;//Fehlererkennung
+					else{
+						$rows['result']=true;
+						$rnum = mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
+						$fnum = mysql_num_fields($res);
+						#liest alle Attribute dynamisch aus --> Anpassungen der Datenbankstruktur möglich
+						for($i=0;$i<$rnum;$i++)
+							for($j=0;$j<$fnum;$j++) $rows[$i][$j]=mysql_result($res,$i,$j);
+					}
+					return $rows;
 				default:
 					return false;
 			}
