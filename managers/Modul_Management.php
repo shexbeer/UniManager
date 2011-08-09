6b07e607e75f8a796373dad2a03d6eccaeaa5fc1
Daniel Rzehak <interraptor@yahoo.de>
2011-08-09 22:44:56 +0200

    eine weitere erweiterung der class Modul_Managemnt
    getter Möglichkeiten stark erweitert falls noch andere gewünscht bitte melden bei Daniel Rzehak
    kleine Änderungen für modulaufstellung nötig aber wegen DBstruktur noch Klärungsbedarf

--- managers/Modul_Management.php
+++ managers/Modul_Management.php
@@ -3,66 +3,113 @@
 ### Bitte auf die Funktionsparameterbeschreibung achten ###
 class Modul_Management{
 	
-	#Abrufen aller Module in der Datenbank mit Name und ID
+	/**
+	* Ruft Name und ID ab aller passender Module ab getModullist([$type, $id[,$semtype,$sem]])
+	* @param string $type Art der ID ['sg','modul']
+	* @param int $id die Id selbst
+	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
+	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
+	*/
 	function getModullist(){
-		$sql = "SELECT modul_name,modul_id FROM modul ORDER BY modul_name";
-		$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
-		return buildResult($res);
+		switch(func_num_args()){
+			case 0: return $this->getModul("modul_name,modul_id");
+			case 2: return $this->getModul("modul_name,modul_id",func_get_arg(0),func_get_arg(1));
+			case 4: return $this->getModul("modul_name,modul_id",func_get_arg(0),func_get_arg(1),func_get_arg(2),func_get_arg(3));
+			default: return false;
+		}
 	}
 	
 	/**
-	* Ruft Moduldetails ab getModuldetails() --alle Module des Tables getModuldetails($type, $id[,$semestertype,$semester]) --bestimmte Module des Tables
+	* Ruft alle Modulatributte ab getModuldetails([$type, $id[,$semtype,$sem]]) --bestimmte Module des Tables
 	* @param string $type Art der ID ['sg','modul']
 	* @param int $id die Id selbst
-	* @param string $semestertype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
-	* @param mixed $semester int für Plansemester, string zB 'SS2011' für kalendarisches Semester
+	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
+	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
 	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
 	*/
 	function getModuldetails(){
-		if(func_num_args()==0){
-			$sql = "SELECT * FROM modul ORDER BY modul_name";
+		switch(func_num_args()){
+			case 0: return $this->getModul("modul.*");
+			case 2: return $this->getModul("modul.*",func_get_arg(0),func_get_arg(1));
+			case 4: return $this->getModul("modul.*",func_get_arg(0),func_get_arg(1),func_get_arg(2),func_get_arg(3));
+			default: return false;
+		}
+	}
+	
+	/**
+	* Achtung sollte nicht direkt aufegrufen werden!! Ruft bestimmte Modulatributte ab getModul($attr,$type, $id[,$semtype,$sem]) --bestimmte Module des Tables
+	* @param string $attr die gesuchten Attribute als String mit Komma getrennt
+	* @param string $type Art der ID ['sg','modul']
+	* @param int $id die Id selbst
+	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
+	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
+	*/
+	private function getModul($attr){
+		if(func_num_args()==1){
+			$sql = "SELECT ".$attr." FROM modul ORDER BY modul_name";
 			$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
 			//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-			return buildResult($res);
+			return $this->buildResult($res);
 		}
 		else{ //wenn Parameter vorhanden ab hier Auswertung
-			$idtype=func_get_arg(0);
-			$id=func_get_arg(1);
+			$idtype=func_get_arg(1);
+			$id=func_get_arg(2);
 			switch($idtype){ // Typenunterscheidung (erster Parameter)
 				case "modul":
-					$sql = "SELECT * FROM modul WHERE modul_id=".$id;
+					$sql = "SELECT ".$attr." FROM modul WHERE modul_id=".$id;
 					$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
 					//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-					return buildResult($res);
+					return $this->buildResult($res);
 				case "sg":
-					if(func_num_args()>2){
-						$semtype=func_get_arg(2);
-						$sem=func_get_arg(3);
+					if(func_num_args()>3){
+						$semtype=func_get_arg(3);
+						$sem=func_get_arg(4);
 						switch($semtype){
 							case "plan":
-								$sql = "SELECT modul.* FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id." AND mauf_plansemester=".$sem." )ON mauf_modul_id=modul_id";
+								$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id." AND mauf_plansemester=".$sem." )ON mauf_modul_id=modul_id";
 								$res = mysql_query($sql);
 								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-								return buildResult($res);
+								return $this->buildResult($res);
 							case "total":
-								$sql = "SELECT modul.* FROM modul INNER JOIN (studiengang INNER JOIN modulangebot ON sg_id=".$id." AND mang_semester=".$sem." )ON mauf_modul_id=modul_id";
+								$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulangebot ON sg_id=".$id." AND ma_semester=".$sem." )ON mauf_modul_id=modul_id";
 								$res = mysql_query($sql);
 								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-								return buildResult($res);
+								return $this->buildResult($res);
 							default:
 								return false;
 							}
 						}
 					else{
-						$sql = "SELECT modul.* FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id.")ON mauf_modul_id=modul_id";
+						$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id.")ON mauf_modul_id=modul_id";
 						$res = mysql_query($sql);
 						//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-						return buildResult($res);
+						return $this->buildResult($res);
 					}
 				default:
 					return false;
 			}
 		}		
+	}
+	
+	/**
+	* Nur eine vereinfachung für mich, Füllt die DB-Resultate in eine array um (array[modulID][attribut])
+	* @param mySql-Rsourcedatei $res
+	* @return  array ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das attribut heißt gleich dem DB-fieldnamen
+	*/
+	private function buildResult($res){
+		if(!$res)$rows['result']=false;//Fehlererkennung
+		else{
+			$rows['result']=true;
+			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
+			$fnum = mysql_num_fields($res);
+			for($i=0;$i<$rnum;$i++){
+					$mod_id = mysql_result($res,$i,"modul_id");
+					for($j=0;$j<$fnum;$j++) $rows[$mod_id][mysql_field_name($res,$j)]=mysql_result($res,$i,$j);
+			}
+		}
+		return $rows;
 	}
 	
 	#Löschen eines Moduls nach ID
@@ -86,17 +133,3 @@ class Modul_Management{
 	
 	
 }
-
-function buildResult($res){
-		if(!$res)$rows['result']=false;//Fehlererkennung
-		else{
-			$rows['result']=true;
-			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
-			$fnum = mysql_num_fields($res);
-			for($i=0;$i<$rnum;$i++){
-					$mod_id = mysql_result($res,$i,"modul_id");
-					for($j=0;$j<$fnum;$j++) $rows[$mod_id][mysql_field_name($res,$j)]=mysql_result($res,$i,$j);
-			}
-		}
-		return $rows;
-	}
