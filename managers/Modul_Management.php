4ad2ca268b1946dbd99e6b77c3c78fc5ce4e7286
Daniel Rzehak <interraptor@yahoo.de>
2011-08-21 21:27:32 +0200

    ACHTUNG: Modul_Management masiv überarbeitet um auch Änderungen einzubeziehen Parameteranpassungen nötig
    SG_management unvollständig

--- managers/Modul_Management.php
+++ managers/Modul_Management.php
@@ -4,88 +4,109 @@
 class Modul_Management{
 	
 	/**
-	* Ruft Name und ID ab aller passender Module ab getModullist([$type, $id[,$semtype,$sem]])
+	* Ruft Name und ID ab aller passender Module ab getModullist($status[,$type, $id[,$semtype,$sem]])
+	  ACHTUNG: status hat keinen default-wert und muss immer mit angegeben werden.
+	* @param bool $status ob Änderung(false) oder fertiges Modul(true)
 	* @param string $type Art der ID ['sg','modul']
 	* @param int $id die Id selbst
 	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
 	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
-	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen unsortiert,false wenn Parameter falsch
 	*/
-	function getModullist(){
-		switch(func_num_args()){
-			case 0: return $this->getModul("modul_name,modul_id");
-			case 2: return $this->getModul("modul_name,modul_id",func_get_arg(0),func_get_arg(1));
-			case 4: return $this->getModul("modul_name,modul_id",func_get_arg(0),func_get_arg(1),func_get_arg(2),func_get_arg(3));
-			default: return false;
+	function getModullist($status,$type=false,$id=false,$semtype=false,$sem=false){
+		if($status){
+			$table="modul";
+			$attr[0]="modul_id";
+			$attr[1]="modul_id";
+			$attr[2]="modul_name";
+			}
+		else{
+			$table="aenderung";
+			$attr[0]="aenderung_id";
+			$attr[1]="aenderung_mid";
+			$attr[2]="aenderung_mname";
 		}
+		return $this->getModul($table,$attr,$type,$id,$semtype,$sem);
 	}
 	
 	/**
-	* Ruft alle Modulatributte ab getModuldetails([$type, $id[,$semtype,$sem]]) --bestimmte Module des Tables
+	* Ruft alle Modulatributte ab getModuldetails($status[,$type, $id[,$semtype,$sem]]) --bestimmte Module des Tables
+	  ACHTUNG: status hat keinen default-wert und muss immer mit angegeben werden.
+	  * @param bool $status ob Änderung(false) oder fertiges Modul(true)
 	* @param string $type Art der ID ['sg','modul']
 	* @param int $id die Id selbst
 	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
 	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
-	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen unsortiert,false wenn Parameter falsch
 	*/
-	function getModuldetails(){
-		switch(func_num_args()){
-			case 0: return $this->getModul("modul.*");
-			case 2: return $this->getModul("modul.*",func_get_arg(0),func_get_arg(1));
-			case 4: return $this->getModul("modul.*",func_get_arg(0),func_get_arg(1),func_get_arg(2),func_get_arg(3));
-			default: return false;
+	function getModuldetails($status,$type=false,$id=false,$semtype=false,$sem=false){
+		if($status){
+			$table="modul";
+			$attr[0]="modul_id";
+			$attr[1]="modul.*";
 		}
+		else{
+			$table="aenderung";
+			$attr[0]="aenderung_id";
+			$attr[1]="aenderung.*";
+		}
+		return $this->getModul($table,$attr,$type,$id,$semtype,$sem);
 	}
 	
 	/**
-	* Achtung sollte nicht direkt aufegrufen werden!! Ruft bestimmte Modulatributte ab getModul($attr,$type, $id[,$semtype,$sem]) --bestimmte Module des Tables
-	* @param string $attr die gesuchten Attribute als String mit Komma getrennt
+	* ACHTUNG: sollte nicht direkt aufegrufen werden!! Ruft bestimmte Modulatributte ab getModul($status,$attr[,$type, $id[,$semtype,$sem]]) --bestimmte Module des Tables
+	* @param string $table aus welchem table werden die Daten geholt (wegen Änderung oder fertiges Modul)
+	* @param string $attr die gesuchten Attribute als array von strings index 0 enthält den Primärschlüsselwertnamen
+	  ACHTUNG: die DBnamenskonvention table_attributname sollte eingehalten werden.
 	* @param string $type Art der ID ['sg','modul']
 	* @param int $id die Id selbst
 	* @param string $semtype ['total'--kalendarisches Semester,'plan'--kalendarisches Semester]
 	* @param mixed $sem int für Plansemester, string zB 'SS2011' für kalendarisches Semester
 	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
 	*/
-	private function getModul($attr){
-		if(func_num_args()==1){
-			$sql = "SELECT ".$attr." FROM modul ORDER BY modul_name";
+	private function getModul($table,$attr,$type=false,$id=false,$semtype=false,$sem=false){
+		$str="";
+		for($i=1;$i<count($attr)-1;$i++)$str=$str.$attr[$i].",";
+		$str=$str.$attr[count($attr)-1];
+		if(!$type){
+			$sql = "SELECT ".$str." FROM ".$table;
 			$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
 			//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-			return $this->buildResult($res);
+			return $this->buildResult($res,$attr[0]);
 		}
 		else{ //wenn Parameter vorhanden ab hier Auswertung
-			$idtype=func_get_arg(1);
-			$id=func_get_arg(2);
+			$idtype=func_get_arg(2);
+			$id=func_get_arg(3);
 			switch($idtype){ // Typenunterscheidung (erster Parameter)
 				case "modul":
-					$sql = "SELECT ".$attr." FROM modul WHERE modul_id=".$id;
+					$sql = "SELECT ".$str." FROM ".$table." WHERE ".$attr[0]."=".$id;
 					$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
 					//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-					return $this->buildResult($res);
+					return $this->buildResult($res,$attr[0]);
 				case "sg":
-					if(func_num_args()>3){
-						$semtype=func_get_arg(3);
-						$sem=func_get_arg(4);
-						switch($semtype){
-							case "plan":
-								$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id." AND mauf_plansemester=".$sem." )ON mauf_modul_id=modul_id";
-								$res = mysql_query($sql);
-								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-								return $this->buildResult($res);
-							case "total":
-								$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulangebot ON sg_id=".$id." AND ma_semester=".$sem." )ON mauf_modul_id=modul_id";
-								$res = mysql_query($sql);
-								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-								return $this->buildResult($res);
-							default:
-								return false;
-							}
-						}
-					else{
-						$sql = "SELECT ".$attr." FROM modul INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id.")ON mauf_modul_id=modul_id";
+					if(!$semtype){
+						$sql = "SELECT ".$str." FROM ".$table." INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id.")ON mauf_modul_id=".$attr[0];
 						$res = mysql_query($sql);
 						//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
-						return $this->buildResult($res);
+						return $this->buildResult($res,$attr[0]);
+					}
+					else{
+						$semtype=func_get_arg(4);
+						$sem=func_get_arg(5);
+						switch($semtype){
+							case "plan":
+								$sql = "SELECT ".$str." FROM ".$table." INNER JOIN (studiengang INNER JOIN modulaufstellung ON sg_id=".$id." AND mauf_plansemester=".$sem." )ON mauf_modul_id=".$attr[0];
+								$res = mysql_query($sql);
+								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
+								return $this->buildResult($res,$attr[0]);
+							case "total":
+								$sql = "SELECT ".$str." FROM ".$table." INNER JOIN (studiengang INNER JOIN modulangebot ON sg_id=".$id." AND ma_semester=".$sem." )ON mauf_modul_id=".$attr[0];
+								$res = mysql_query($sql);
+								//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
+								return $this->buildResult($res,$attr[0]);
+							default:
+								return false;
+						}
 					}
 				default:
 					return false;
@@ -98,14 +119,14 @@ class Modul_Management{
 	* @param mySql-Rsourcedatei $res
 	* @return  array ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das attribut heißt gleich dem DB-fieldnamen
 	*/
-	private function buildResult($res){
+	private function buildResult($res,$key){
 		if(!$res)$rows['result']=false;//Fehlererkennung
 		else{
 			$rows['result']=true;
 			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
 			$fnum = mysql_num_fields($res);
 			for($i=0;$i<$rnum;$i++){
-					$mod_id = mysql_result($res,$i,"modul_id");
+					$mod_id = mysql_result($res,$i,$key);
 					for($j=0;$j<$fnum;$j++) $rows[$mod_id][mysql_field_name($res,$j)]=mysql_result($res,$i,$j);
 			}
 		}
--- managers/SG_Management.php
+++ managers/SG_Management.php
@@ -26,28 +26,98 @@ class SG_Management {
 	
 	///getter///
 	
-	//Liste aller Studiengaenge
-	function getSGlist(){
-		$sql = "SELECT sg_name, sg_id FROM studiengang ORDER BY sg_name";
-		$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
-		if(!$res)$rows['result']=false; //dient aktuell zur Fehlererkennung
-		else{
-			$rows['result']=true;
-			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
-			for($i=0;$i<$rnum;$i++){
-				$mod_id = mysql_result($res,$i,"sg_id");
-				$rows[$mod_id]['name']=mysql_result($res,$i,"sg_name");
-				$rows[$mod_id]['id']=$mod_id;
-			}
+	/**
+	* Achtung sollte nicht direkt aufegrufen werden!! Ruft bestimmte atributte eines SG ab (Vorlage aus Modul_Management) getSG($attr,$id)
+	  stark vereinfacht, da nach Sequenzdiagramm immer nur die allgemeine Liste oder ein bestimmter SG abgerufen wird --> keine JOINTS nötig
+	* @param string $attr die gesuchten Attribute als String mit Komma getrennt
+	* @param string $id 
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das atribut heißt gleich dem DB-namen geordnet dem Attributnamen nach,false wenn Parameter falsch
+	*/
+	private function getSG($attr){
+		if(func_num_args()==1){
+			$sql = "SELECT ".$attr." FROM studiengang ORDER BY sg_name";
+			$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
+			//2dim array array[sg_id][attribut] id ist in den attributen ebenfalls vorhanden 
+			return $this->buildResult($res,"sg_id");
 		}
-		return $rows;
+		else{ //wenn Parameter vorhanden ab hier Auswertung
+			$id=func_get_arg(1);
+			$sql = "SELECT ".$attr." FROM modul WHERE sg_id=".$id;
+			$res = mysql_query($sql);# false wenn Entity oder Attribut nicht existiert
+			
+			//2dim array array[modulID][attribut] id ist in den attributen ebenfalls vorhanden 
+			return $this->buildResult($res,"sg_id");
+			}*/
+		}		
 	}
+	
+	/**
+	*/
+	function getSO(){
+	}
+	
+	/**
+	*/
+	function getPO(){
+	}
+	
+	/**
+	*/
+	function getSIDListSG(){
+	}
+	
+	
+	/**
+	*Ruft Liste aller SG ab
+	 Ruft Name und ID ab aller passender Module ab getModullist([$id],[$status])
+	* @param int $id die SG_ID
+	* @param string $status optional für um nur SG mit einem bestimmten Status zu wählen  
+	* @return mixed array mit DB-Resultaten, ['result'] enthält false bei DB-Fehler, array[sg_id][sg_name] das atribut heißt gleich dem DB-namen geordnet dem attribut Namen nach,false wenn Parameter falsch
+	*/
+	function getSGList(){
+		switch(func_num_args()){
+			case 0: return $this->getSG("sg_name,sg_id");
+			case 1: return $this->getSG("sg_name,sg_id",func_get_arg(0));
+			default: return false;
+		}
+	}
+	
+	/**
+	*/
+	function getSGDetails(){
+		switch(func_num_args()){
+			case 0: return $this->getModul("studiengang.*");
+			case 1: return $this->getModul("studiengang.*",func_get_arg(0));
+			default: return false;
+	}
+	
 	
 	//Inhalt ist mir nicht ganz klar --Diskusionsbedarf--
 	function getTamplate(){
 	}
 	
-	//
+	///Helper///
+	
+	/**
+	* Erneut die buildResult aus Modul_Management, Füllt die DB-Resultate in eine array um (array[modulID][attribut])
+	* @param mySql-Rsourcedatei $res
+	* @return  array ['result'] enthält false bei DB-Fehler, array[modulID][attribut] das attribut heißt gleich dem DB-fieldnamen
+	*/
+	private function buildResult($res,$keyvalue){
+		if(!$res)$rows['result']=false;//Fehlererkennung
+		else{
+			$rows['result']=true;
+			$rnum=mysql_num_rows($res);# 0 wenn kein Treffer gefunden wurde
+			$fnum = mysql_num_fields($res);
+			for($i=0;$i<$rnum;$i++){
+					$mod_id = mysql_result($res,$i,$keyvalue);
+					for($j=0;$j<$fnum;$j++) $rows[$mod_id][mysql_field_name($res,$j)]=mysql_result($res,$i,$j);
+			}
+		}
+		return $rows;
+	}
+	
+	
 }
 
 ?>
