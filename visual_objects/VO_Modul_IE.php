e56858d67a9f8f55bebc192d7c579a9292296c5a
wangfei79 <wangfei7911@yahoo.de>
2011-08-24 13:18:43 +0300

    Edited visual_objects/VO_Modul_IE.php via GitHub

--- visual_objects/VO_Modul_IE.php
+++ visual_objects/VO_Modul_IE.php
@@ -8,6 +8,11 @@ class VO_Modul_IE
 	}
 	function showResult()
 	{
+		$this->UM->tpl->assign("error", $error_occurred);
+                $this->UM->tpl->assign("extra_msg", $extra_message);
+                $this->UM->showfooter();
+                $this->UM->showheader($this->UM->seite);
+                $this->UM->tpl->display("Modul_IE_Result.tpl", session_id());
 	}
 	function showModulList($mDetails)	// shex: fuer Fei, diese Fkt hab ich dir schonmal geschrieben inklusive der Template Datei, versuche was daraus zu lernen
 	{
